9841af7c72e7d0e9d5d98d1dd3ac85ca7427564f
Jean-Baptiste de Vulder <devulder.jeanbaptiste@gmail.com>
2025-09-11 15:18:47 +0200

    feat: navbar
    navbar added, homepage started

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,6 @@
-import './bootstrap.js';
-/*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
- */
+// Bootstrap CSS
+import './styles/bootstrap.min.css'
+// Bootstrap JS
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+// Custom CSS
 import './styles/app.css';
-
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
